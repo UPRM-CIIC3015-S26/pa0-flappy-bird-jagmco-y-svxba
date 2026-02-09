41a19580e35d286b4c3d22741a9b5a802398f53e
00colon <your@email.com>
2026-02-09 19:24:48 -0400

    Task 1

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame, random
 pygame.init()
+pygame.mixer.init()
+score_sound = pygame.mixer.Sound("sound.wav")
+crash_sound = pygame.mixer.Sound("crash.wav")
 '''
 Welcome to PA0 – Flappy Bird! Throughout this code, you are going to find a recreation of a game you have probably
 heard of before. This is an introductory assignment designed to help you familiarize yourself with what you can expect 
@@ -16,7 +19,7 @@ pygame.display.set_caption("Flappy Bird")
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
-PLAYER = (255, 255, 0)
+PLAYER = (255, 200, 0)
 
 # Font Size -->
 big_font = pygame.font.SysFont(None, 80)
@@ -38,20 +41,20 @@ bird_y = 300
 bird_velocity = 0
 # TODO 1: Tweaking the physics
 # Looks like the player is falling too quickly not giving a change to flap it's wing, maybe tweak around with the value of this variable
-gravity = 9.81
-jump = -10
+gravity = 0.5
+jump = -8
 # Pipe Variables -->
 pipe_x = 400
 pipe_width = 70
 # TODO 2.1: A Little gap Problem
 # You probably noticed when running the code that it's impossible the player to go through the gaps
 # play around with the pipe_gap variable so that its big enough for the player to pass through
-pipe_gap = 170
+pipe_gap = 150
 pipe_height = random.randint(100, 400)
 # TODO 2.2: The too fast problem
 # The pipes are moving way too fast! Play around with the pipe_speed variable until you find a good
 # speed for the player to play in!
-pipe_speed = 20
+pipe_speed = 3
 
 score = 0
 game_over = False
@@ -63,7 +66,7 @@ running = True
 while running:
     # TODO 6: Changing the name!
     # D'oh! This is not yout name isn't follow the detailed instructions on the PDF to complete this task.
-    name = "José García"
+    name = "Sebastián A. Colón"
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -80,7 +83,6 @@ while running:
                     # It is your job to find why this is happening! (Hint: What variable stores the y coordinates
                     # of the bird)
                     bird_velocity = 0
-                    bird_y = 300
                     pipe_x = 400
                     score = 0
                     game_over = False
@@ -95,10 +97,11 @@ while running:
         if pipe_x < -70:
             pipe_x = 400
             pipe_height = random.randint(100, 400)
+            score_sound.play()
             # TODO 4: Fixing the scoring
             # When you pass through the pipes the score should be updated to the current score + 1. Implement the
             # logic to accomplish this scoring system.
-            score = 1
+            score = score + 1
 
         if bird_y > 600 or bird_y < 0:
             game_over = True
@@ -108,6 +111,7 @@ while running:
         bottom_pipe_rect = pygame.Rect(pipe_x, pipe_height + pipe_gap, pipe_width, 600)
 
         if bird_rect.colliderect(top_pipe_rect) or bird_rect.colliderect(bottom_pipe_rect):
+            crash_sound.play()
             game_over = True
 
     screen.fill(pygame.Color('grey12'))
